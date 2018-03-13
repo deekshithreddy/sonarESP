286c14308d3c8911c669b4b9a9b8c4f9df420c44
Deekshith Reddy Vangala <deekshithreddy22394@gmail.com>
2018-03-13 12:51:24 +0530

    initial commit. ESP project initialized.

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+
+const char * ssid = "Deekshu:iPhone";
+const char * password = "123456789";
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(9600);
+  setup_wifi();
+  
+}
+
+void loop() {
+
+}
