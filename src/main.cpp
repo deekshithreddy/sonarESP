d73463adb85e69c6d91d1e842cb723757cdb81df
Deekshith Reddy Vangala <deekshithreddy22394@gmail.com>
2018-03-13 15:13:14 +0530

    code changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-const char * ssid = "Deekshu:iPhone";
-const char * password = "123456789";
+const char * SSID = "Deekshu:iPhone";
+const char * PASSWORD = "123456789";
+
+const char* HOST = "sonarapi";
+const int PORT = 80;
 
 void setup_wifi() {
   delay(10);
   Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -23,9 +26,41 @@ void setup_wifi() {
 void setup() {
   Serial.begin(9600);
   setup_wifi();
-
 }
 
 void loop() {
+  delay(5000);
+  Serial.print("connecting to ");
+  Serial.println(HOST);
+  WiFiClient client;
+  if (!client.connect(HOST, PORT)) {
+    Serial.println("connection failed");
+    return;
+  }
 
+  String url = "/input/";
+  url += "&value";
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + HOST + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+
+  while (client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
 }
